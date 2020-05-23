76966f9e2fb378ef2586afe212bcf03bef54e6de
Abul Hashem Jr <abul@limosys.com>
2020-05-22 20:43:38 -0400

    Initial commit

---
+++ app/src/main/java/com/project/java/a20200522_abulhashemjr_nycschools/NYCSchoolsApp.java
@@ -0,0 +1,10 @@
+package com.project.java.a20200522_abulhashemjr_nycschools;
+
+import android.app.Application;
+
+public class NYCSchoolsApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/com/project/java/a20200522_abulhashemjr_nycschools/NYCSchoolsMainActivity.java
@@ -0,0 +1,14 @@
+package com.project.java.a20200522_abulhashemjr_nycschools;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NYCSchoolsMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
